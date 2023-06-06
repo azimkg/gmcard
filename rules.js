e0e9b6579955ee385d0f80b57fdf266b9d6385d9
azimkg <azeldenbaev@gmail.com>
2023-06-07 01:04:23 +0600

    add help and rules pages

---
+++ rules.js
@@ -0,0 +1,16 @@
+const gm = document.querySelector("#gm")
+const user_conf = document.querySelector("#user_conf")
+const rules_team = document.querySelector("#rules_team")
+const possibilities = document.querySelector("#possibilities")
+const about_sales = document.querySelector("#about_sales")
+const about_contest = document.querySelector("#about_contest")
+const about_tasks = document.querySelector("#about_tasks")
+const about_p2p = document.querySelector("#about_p2p")
+const black_list = document.querySelector("#black_list")
+const about_barter = document.querySelector("#about_barter")
+const about_things = document.querySelector("#about_things")
+const about_crowdfunding = document.querySelector("#about_crowdfunding")
+
+// user_conf.addEventListener("click", () => {
+//     user_conf.classList.add("activate")
+// })
