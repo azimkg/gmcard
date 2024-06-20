93a1f35a8582756f9c2ec75fac0700db92026b27
azimkg <azeldenbaev@gmail.com>
2024-06-21 03:12:01 +0600

    upload photo to page profile

--- js/profile.js
+++ js/profile.js
@@ -62,4 +62,69 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('closeModal').addEventListener('click', () => {
         document.getElementById('modalFromService').classList.add('hidden')
     })
+
+    document.getElementById('photoBlock').addEventListener('mouseenter', () => {
+        document.getElementById('photoChangeBtn').classList.remove('hidden')
+    })
+
+    document.getElementById('photoBlock').addEventListener('mouseleave', () => {
+        document.getElementById('photoChangeBtn').classList.add('hidden')
+    })
+
+    document.getElementById('photoChangeBtn').addEventListener('click', function () {
+        document.getElementById('uploadImage2').click();
+    });
+
+    document.getElementById('uploadImage2').addEventListener('change', function (event) {
+        var file = event.target.files[0];
+        var image = document.getElementById('cropperImage2');
+
+        if (file) {
+            // Проверка размера файла (не больше 10 МБ)
+            var maxSize = 10 * 1024 * 1024; // 10 МБ в байтах
+
+            if (file.size > maxSize) {
+                alertFile.classList.add("block");
+                alertFile.classList.remove("hidden");
+                event.target.value = null;
+                return;
+            }
+
+            // Продолжение кода обработки изображения
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                image.src = e.target.result;
+
+                var cropper = new Cropper(image, {
+                    aspectRatio: 1,
+                    viewMode: 0,
+                    autoCropArea: 0.5,
+                    dragMode: "none",
+                    cropBoxResizable: true,
+                    background: false,
+                    zoomable: false,
+                    guides: false
+                });
+
+                document.getElementById('cropModal2').style.display = 'block';
+
+                document.getElementById('cropButton2').addEventListener('click', function () {
+                    var croppedCanvas = cropper.getCroppedCanvas();
+                    var croppedImage = croppedCanvas.toDataURL();
+                    document.getElementById('cropperResult2').style.display = "block";
+                    document.getElementById('cropperResult2').src = croppedImage;
+                    document.getElementById('cropModal2').style.display = 'none';
+
+                    cropper.destroy();
+                });
+
+                document.getElementById('cancelButton2').addEventListener('click', function () {
+                    document.getElementById('cropModal2').style.display = 'none';
+                    cropper.destroy();
+                });
+            };
+
+            reader.readAsDataURL(file);
+        }
+    });
 });
